e869ec05d69c4736c3a7d5434d1db644fab970b7
Zappar <hello@zappar.com>
2020-12-11 16:09:29 +0000

    Minimal webpack project for Zappar react-three-fiber image tracking

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js'
+	},
+	resolve: {
+		extensions: [".js", ".wasm"]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html'
+		})
+	],
+	devServer: {
+		contentBase: './dist',
+		https: true,
+		host: '0.0.0.0'
+	},
+	module: {
+		rules: [{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /zcv\.wasm$/,
+				type: "javascript/auto",
+				loader: "file-loader"
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				},
+				options: {
+					presets: [
+						"@babel/preset-env",
+						"@babel/preset-react"
+					]
+				},
+			}
+		]
+	}
+};
